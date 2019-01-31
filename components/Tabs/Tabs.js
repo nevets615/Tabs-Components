05a9f14a7e4160345998d1873137f53e3d90fadf
nevets615 <surj316@yahoo.com>
2019-01-31 17:31:08 -0500

    working through it. got almost all the tabs done. still need to see whats wrong with my dropdown

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,12 +5,14 @@ class TabLink {
     
     this.element = element;
     // Get the custom data attribute on the Link
-    
+    this.data = element.dataset.tab;
+
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
-    this.data =  document.querySelector(`.tabs-item[data-tab='${link.dataset.tab}']`);;
+    // ;
+     this.itemElement =  document.querySelector(`.tabs-item[data-tab='${element.dataset.tab}']`);;
     // Using the Item element, create a new instance of the TabItem class
+    
     this.tabItem = new Content(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
@@ -20,10 +22,10 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = elem.querySelectorAll(".tabs-link"); 
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    links = Array.from(links).forEach(link.removeAttribute("class", "tabs-link-selected"));
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
@@ -41,7 +43,7 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    // const items = ;
 
     // Remove the class "tabs-item-selected" from each element
     
